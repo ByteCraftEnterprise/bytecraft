8c6e8220d0a4dced97c49aa28f1412296334cf2a
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-08-25 20:13:13 -0400

    Installed Bootstrap

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import "../styles/navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top">
+            <div className="container">
+                <a className="navbar-brand" href="/">ByteCraft</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/bytecraft">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/programs">Programs</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
