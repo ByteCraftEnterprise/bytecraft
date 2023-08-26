6f0b79908f68d4e281239d00bc6f3448eaa4b014
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-08-25 20:25:36 -0400

    Create goals.js

---
+++ src/goals.js
@@ -0,0 +1,39 @@
+const goals = [
+    {
+        id: 1,
+        title: "Increase Access to Computer Science Education:",
+        description1: "The primary goal of ByteCraft is to provide access to quality education in computer science to underserved communities.",
+        description2: "We aim to bridge the digital divide by offering courses and resources to individuals who may not have easy access to traditional educational institutions or resources",
+        logo: "https://iili.io/HbFSjwX.png"
+    },
+    {
+        id: 2,
+        title: "Community Building:",
+        description1: "Learning a new skill can be difficult, but with the right community to guide you along the way, learning is a lot more fun. ",
+        description2: "Here at ByteCraft, we strive to foster a community where students can see others’ progress along the way and be inspired and build upon projects being created within the community",
+        logo: "https://iili.io/HbFrUSR.png"
+    },
+    {
+        id: 3,
+        title: "Encourage Diversity and Inclusion:",
+        description1: "We hope to increase diversity and inclusion in undeserved communities by providing tools and resources students may not have had otherwise.",
+        description2: "With a motivated team ready to help students accomplish their project dreams, we aim to continue to provide students with resources and projects that will add a competitive edge to their college educations.",
+        logo: "https://iili.io/HbFrPtt.png"
+    },
+    {
+        id: 4,
+        title: "Empowerment and Skill Development:",
+        description1: "We aim to empower students by equipping them with the knowledge and skills necessary to thrive in STEM related fields, while adding a competitive edge to their applications through skill and project building.",
+        description2: "ByteCraft provides opportunities for students to learn programming languages, algorithms, data structures, software development methodologies, and other essential concepts in computer science while working on their own projects.",
+        logo: "https://iili.io/HbFrwt1.png"
+    },
+    {
+        id: 5,
+        title: "Provide Mentorship and Support:",
+        description1: "We acknowledge that a community that fosters mentorship and support can significantly enhance the effectiveness of the program.",
+        description2: "Therefore, here at ByteCraft we provide zoom office hours for students to come and talk to undergraduate students in computer science who can guide them through the learning process, provide career advice, and offer support for individual projects. This can greatly benefit underserved populations, who may not have access to such resources otherwise.",
+        logo: "https://iili.io/HbFrtKG.png"
+    },
+]
+
+export default goals;
