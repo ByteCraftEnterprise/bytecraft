519ddb36c015f4a96f152e0a16ea045fda97578f
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-09-01 11:58:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="/bytecraft-website">
       <Navbar />
       <Routes>
         <Route path="*" element={<Homepage />} />
